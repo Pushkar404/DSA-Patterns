3cbe62bee7a4152f6775df6e800d5404563b5ed2
Pushkar404 <pushkar8705@gmail.com>
2026-08-25 23:54:41 +0530

    done this will make notes

---
+++ fast-and-slow-pointer/gfg-split-a-circular-linked-list-into-two-halves.cpp
@@ -0,0 +1,35 @@
+/* The structure of linked list Node*/
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+  int data;
+  struct Node *next;
+
+  Node(int x){
+      data = x;
+      next = nullptr;
+  }
+};
+
+
+class Solution {
+  public:
+    pair<Node *, Node *> splitList(struct Node *head) {
+          Node* slow = head;
+          Node* fast = head->next;
+
+          while (fast != head || fast->next != head){
+            slow = slow->next;
+            fast = fast->next;
+
+            if(fast->next != head){
+                  fast = fast->next;
+            }
+          }
+          
+          fast->next = slow->next;
+          slow->next = head;
+        
+    }
+};
